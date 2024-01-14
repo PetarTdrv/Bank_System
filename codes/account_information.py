c35bf454205fa995e13082b92aba344a5f88be63
Petar Todorov <154418284+PetarTdrv@users.noreply.github.com>
2024-01-14 23:34:57 +0200

    Create account_information.py

---
+++ codes/account_information.py
@@ -0,0 +1,4 @@
+owner = input("Enter your name: ")
+start_balance = float(input("Enter start balance: "))
+type_of_currency = input("Enter currency of card('BGN', 'USD', 'EUR'): ").lower()
+country = input("Enter place of card('bg', 'de', 'fr', 'it', 'gb'): ").lower()
